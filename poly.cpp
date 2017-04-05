0cff31ca5b06ca35400a79cd70d847126e80a23a
rolan <rajmund977@gmail.com>
2017-04-05 12:02:58 +0200

    v0.01
    new operators: +=, -=, +, -
    operator<< fixed

--- poly.cpp
+++ poly.cpp
@@ -1,28 +1,79 @@
 #include "poly.hpp"
 
-
-std::ostream& operator<<(std::ostream& out, poly p) {
-	int d = p.deg;
-	for (std::vector<int>::iterator it = p.coeff.begin() ; it != p.coeff.end(); ++it){
-		if (d > 0)
-			out << *it << "x^" << d-- << " + ";
-		else
-			out << *it;
+//todo: ha az együttható 1, akkor csak simán x^d legyen, ne 1x^d
+//		negatív számokra is jól nézzen ki (5x^2 - 2x - 1)
+std::ostream& operator<<(std::ostream& out, const poly p) {
+	unsigned int d = p.deg;
+	switch (d) {
+		default:
+			while(d > 1) {
+				if (p.coeff[d] != 0) 
+					out << p.coeff[d] << "x^" << d << " + ";
+				d--;
+			}
+		case 1:
+			if(p.coeff[d] != 0)
+				out << p.coeff[d] << "x + ";
+		case 0:
+			out << p.coeff[d];
+			break;			
 	}
 	return out;
 }
 
+poly& poly::operator+=(const poly& rhs) {
+	if (rhs.deg > deg) {
+		for (int i = 0; i <= deg; i++) {
+			coeff[i] += rhs.coeff[i];
+		}
+		for (int i = deg+1; i <= rhs.deg; i++) {
+			coeff.push_back(rhs.coeff[i]);
+		}		
+	} else
+	{
+		for (int i = 0; i <= rhs.deg; i++) {
+			coeff[i] += rhs.coeff[i];
+		}
+	}	
+	return *this;
+}
+
+const poly poly::operator+(const poly& rhs) const {
+	poly result = *this;
+	result += rhs;
+	return result;
+}
+
+poly& poly::operator-=(const poly& rhs) {
+	if (rhs.deg > deg) {
+		for (int i = 0; i <= deg; i++) {
+			coeff[i] -= rhs.coeff[i];
+		}
+		for (int i = deg+1; i <= rhs.deg; i++) {
+			coeff.push_back(-rhs.coeff[i]);
+		}		
+	} else
+	{
+		for (int i = 0; i <= rhs.deg; i++) {
+			coeff[i] -= rhs.coeff[i];
+		}
+	}	
+	return *this;
+}
+
+const poly poly::operator-(const poly& rhs) const {
+	poly result = *this;
+	result -= rhs;
+	return result;
+}
+
 int main() {
 	
-	std::vector<int> v;
-	for (int i = 1; i < 6; i++) {
-		v.push_back(i);
-	}
+	std::vector<int> v(5);
+	poly sajtban(v);
+	poly copy = sajtban - sajtban;
+	poly test;
 	
-	poly sajtban(v.size()-1, v);
-	
-	std::cout << sajtban << '\n';
-	std::cout << "Nyomj egy gombot a kilepeshez!\n"; 
-	std::cin.get();
+	std::cout << copy << " " << test << '\n';
 
 }
--- poly.hpp
+++ poly.hpp
@@ -6,11 +6,15 @@
 
 class poly {
 public:
-	poly(const int d, const std::vector<int> v) : deg(d), coeff(v) {}; 
-	friend std::ostream& operator<<(std::ostream& out, poly p);
-
+	poly() : deg(0), coeff{0} {};
+	poly(const std::vector<int> v) : coeff(v), deg(v.size()-1) {};
+	friend std::ostream& operator<<(std::ostream& out, const poly p);
+	poly& operator+=(const poly &rhs);
+	poly& operator-=(const poly& rhs);
+	const poly operator+(const poly& rhs) const;
+	const poly operator-(const poly& rhs) const;
 private:
-	int deg;
+	unsigned int deg;
 	std::vector<int> coeff;
 };
 
